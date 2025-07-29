1093948a8b6284a05c675b8f56ab16fd3c51975a
Grant Moore <gmoore@Grants-MacBook-Pro.local>
2025-07-29 12:20:17 -0400

    update module sources

--- modules.tf
+++ modules.tf
@@ -1,7 +1,7 @@
 module "platforms" {
   for_each              = local.platforms
 
-  source                = "github.com/cumberland-terraform/network-vpc.git"
+  source                = "github.com/cumberland-terraform/platform.git"
 
   platform              = each.value
 }
